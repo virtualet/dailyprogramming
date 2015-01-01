7a1d2cce93c0a5dbcb23162af9413a46ee903086
Eric Cho <echoecho@gmail.com>
2014-12-31 19:51:09 -0600

    update for comments

--- challenge003.py
+++ challenge003.py
@@ -1,3 +1,6 @@
+# write a program that can encrypt texts with an alphabetical caesar cipher.
+# This cipher can ignore numbers, symbols, and whitespace.
+# For extra credit, add a "decrypt" function to your program!
 __author__ = 'echoecho'
 
 option = ''
